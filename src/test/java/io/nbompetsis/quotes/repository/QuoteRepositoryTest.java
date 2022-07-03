1eccd5c5f1b161a1dd7a7a1cc82881c8752f5806
Nikolas Bompetsis <nbompetsis@di.uoa.gr>
2022-07-04 02:33:14 +0300

    Minor changes on jpa repository tests

--- src/test/java/io/nbompetsis/quotes/repository/QuoteRepositoryTest.java
+++ src/test/java/io/nbompetsis/quotes/repository/QuoteRepositoryTest.java
@@ -7,12 +7,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.test.context.TestPropertySource;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@TestPropertySource(properties = {
+        "spring.jpa.hibernate.ddl-auto=create-drop"
+})
 class QuoteRepositoryTest {
 
     @Autowired
